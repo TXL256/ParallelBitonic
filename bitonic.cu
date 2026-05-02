fa42ce1de25f551e3be70788a1e9b6e7f5f28cbb
TXL256 <scottkeeter@mines.edu>
2026-05-02 01:29:09 -0500

    serial implementation added

--- bitonic.cu
+++ bitonic.cu
@@ -2,16 +2,70 @@
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
+#include <algorithm>
+#include <bit>
+#include <bitset>
+#include <limits.h>
 
 #include "util.h"
 
-
-int * serial_implementation(int * input, int N){
-    //TODO: serial implementation
+//inplace
+//start inclusive, end exclusive
+//direction: 0=descending, 1=ascending
+void comp_swap(int * data, int i1, int i2, bool direction){ 
+    if (!direction && i1<i2 || direction && i1>i2){
+        int temp = data[i1];
+        data[i1] = data[i2];
+        data[i2] = temp;
+    }
 }
 
-int * control_implementation(int * input, int N){
+//inplace
+void bitonic_merge(int * data, int start_i, int end_i, bool direction){
+    int section_len = end_i-start_i;
+    if (section_len <= 1) {return;}
+    for (int i = 0; i < section_len/2; i++){
+        comp_swap(data, i, i+section_len/2, direction);
+    }
+    int mid_i = (start_i + end_i) / 2;
+    bitonic_merge(data, start_i, mid_i, direction);
+    bitonic_merge(data, mid_i, end_i, direction);
+}
+
+void bitonic_sort(int * data, int N, int start_i, int end_i, bool direction){
+    int section_len = end_i-start_i;
+    if (section_len <= 1){return;}
+    int mid_i = (start_i + end_i) / 2;
+    bitonic_sort(data, N, start_i, mid_i, 1);
+    bitonic_sort(data, N, mid_i, end_i, 0);
+    bitonic_merge(data, start_i, end_i, direction);
+}
+
+void serial_implementation(int * input, int * output, int N){
+    //TODO: serial implementation
+    //make a MAXINT padded array to force array len to be power of 2
+    int pow_2_cap = std::__bit_ceil(N);
+    int * working = (int*) malloc(sizeof(int) * pow_2_cap);
+    for (int i = 0; i < N; i++){
+        working[i] = input[i];
+    }
+    for (int i = N; i < pow_2_cap; i++){
+        working[i] = INT_MAX;
+    }
+    bitonic_sort(working, pow_2_cap, 0, pow_2_cap, 1);
+    for (int i = 0; i < N; i++){
+        output[i] = working[i];
+    }
+    free(working);
+    return;
+}
+
+void control_implementation(int * input, int * output, int N){
     //TODO: control implementation
+    for (int i = 0; i < N; i++){
+        output[i] = input[i];
+    }
+    std::sort(output, output+N);
 }
 
 //in: name of file to read an array of ints from
@@ -30,14 +84,18 @@ int main(int argc, char ** argv) {
     cudaEventCreate(&begin);
     cudaEventCreate(&end);
 
-    int * parallel_sorted;
 
     //TODO: sort using imported algorithm
+    int * control_sorted = (int*) malloc(sizeof(int) * N);
+    control_implementation(data, control_sorted, N);
 
     //TODO: sort using serial algorithm
+    int * serial_sorted = (int*) malloc(sizeof(int) * N);
+    serial_implementation(data, serial_sorted, N);
 
     //TODO: sort using parallel algorithm
     //TODO: var definitions
+    int * parallel_sorted = (int*) malloc(sizeof(int) * N);
     //TODO: device mem allocation
     //TODO: memory transfer
     //TODO: kernel launch
@@ -48,10 +106,8 @@ int main(int argc, char ** argv) {
     cudaEventElapsedTime(&ms, begin, end);
     printf("Elapsed time: %f ms\n", ms);
 
-    //TODO: deallocate resources
 
-    int * control_sorted = control_implementation(data, N);
-    int * serial_sorted = serial_implementation(data, N);
+
     for (int i=0; i < N; i++){
         if (control_sorted[i] != serial_sorted[i]) {
             printf("ERROR; serial incorrect: %d != %d @ %d\n", control_sorted[i], serial_sorted[i], i);
@@ -66,7 +122,11 @@ int main(int argc, char ** argv) {
     cudaEventDestroy(end);
     cudaStreamDestroy(stream);
 
+    //TODO: deallocate resources
     free(data);
+    free(control_sorted);
+    free(serial_sorted);
+    free(parallel_sorted);
 
     return 0;
 }
